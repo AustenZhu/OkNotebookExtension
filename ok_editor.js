a54fa27a4135520c8225a8b9009287aa86524ff1
austenzhu <austenzhu@berkeley.edu>
2017-08-23 13:21:22 -0700

    some jquery

--- ok_editor.js
+++ ok_editor.js
@@ -6,7 +6,12 @@ define(['require', 'jquery', 'base/js/namespace'], function (require, $, IPython
     }
 
     var create_ok_buttons = function() {
-
+        //finding the location of all grading cells
+        $('#notebook').find('.cm-property').map(function(i,prop) {
+            if(prop.innerText === 'grade') {
+                //Adding button div here, adjacent to grading cell
+            };
+        });
     }
 
     var ok = function() {
@@ -19,9 +24,13 @@ define(['require', 'jquery', 'base/js/namespace'], function (require, $, IPython
         ok();
     };
 
-    var ok_button = function(){
+    var ok_button = function() {
+        
+    };
+
+    var ok_toolbar_button = function(){
         if (!IPython.toolbar) {
-            $([IPython.events]).on('app_initialized.NotebookApp', ok_button); 
+            $([IPython.events]).on('app_initialized.NotebookApp', ok_toolbar_button); 
             return; 
         }
         if ($('ok_button').length === 0) {
